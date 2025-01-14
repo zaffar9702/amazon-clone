b01f53514970d97ccd51cd738df8ee2b32d7587c
zaffar ansari <zaffaransari63@gmail.com>
2025-01-14 10:34:01 +0530

    add changes

--- index.js
+++ index.js
@@ -1,42 +1,66 @@
 const imgs = document.querySelectorAll('.header-slider ul img')
 const prev_btn = document.querySelector('.control_prev')
 const next_btn = document.querySelector('.control_next')
+const navCart = document.querySelector('.nav-cart')
+const product = document.querySelector('.products')
+const navText = document.querySelector('.nav-text')
+const signup_btn = document.querySelector('.signin-signup-btn')
 
 let n = 0;
 
-function changeSlide(){
+function changeSlide() {
     for (let i = 0; i < imgs.length; i++) {
         imgs[i].style.display = 'none';
-        
+
     }
     imgs[n].style.display = 'block';
 }
 
 changeSlide();
 
-prev_btn.addEventListener('click',(e)=>{
-    if(n > 0){
+prev_btn.addEventListener('click', (e) => {
+    if (n > 0) {
         n--;
-    }else{
+    } else {
         n = imgs.length - 1;
     }
     changeSlide();
 })
 
-next_btn.addEventListener('click',(e)=>{
-    if(n <  imgs.length - 1){
+next_btn.addEventListener('click', (e) => {
+    if (n < imgs.length - 1) {
         n++;
-    }else{
+    } else {
         n = 0;
     }
     changeSlide();
 })
 
 const scrollContainer = document.querySelectorAll('.products');
-for(const item of scrollContainer){
-    item.addEventListener('wheel', (evt)=>{
+for (const item of scrollContainer) {
+    item.addEventListener('wheel', (evt) => {
         evt.preventDefault();
         item.scrollLeft += evt.deltaY;
     });
 
 }
+
+
+navCart.addEventListener("click", () => {
+    window.location = "/cart.html"
+})
+
+
+
+product.addEventListener("click", () =>{
+    window.location = "/product.html"
+})
+
+navText.addEventListener("click", () =>{
+    window.location = "/signin.html"
+})
+
+
+signup_btn.addEventListener("click", () =>{
+    window.location = "/signup.html"
+})
